6a88e3c64d70b90baf700d7611dec39e96536b46
Aaron Miller <aaron@aaron-miller.net>
2023-04-16 00:17:29 -0500

    Formatting

--- awesome/.config/awesome/rc.lua
+++ awesome/.config/awesome/rc.lua
@@ -81,10 +81,7 @@ awful.layout.layouts = {
 -- {{{ Menu
 -- Create a launcher widget and a main menu
 myawesomemenu = {
-    {
-        "hotkeys",
-        function() hotkeys_popup.show_help(nil, awful.screen.focused()) end
-    }, {"manual", terminal .. " -e man awesome"},
+    {"hotkeys", function() hotkeys_popup.show_help(nil, awful.screen.focused()) end }, {"manual", terminal .. " -e man awesome"},
     {"edit config", editor_cmd .. " " .. awesome.conffile},
     {"restart", awesome.restart}, {"quit", function() awesome.quit() end}
 }
