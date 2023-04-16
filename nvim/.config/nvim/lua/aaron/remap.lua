73c80646d84d052cf4d45ef98a0742eb3daa9ed7
Aaron Miller <aaron@aaron-miller.net>
2023-04-15 21:14:47 -0500

    Added more binds

--- nvim/.config/nvim/lua/aaron/remap.lua
+++ nvim/.config/nvim/lua/aaron/remap.lua
@@ -5,7 +5,9 @@ vim.keymap.set("n", "<leader>w", ':w<CR>', {noremap = true})
 vim.keymap.set("n", "<leader>q", ':q<CR>', {noremap = true})
 vim.keymap.set("n", "<leader>Q", ':q!<CR>', {noremap = true})
 vim.keymap.set('n', '<leader>ev', ':vsplit $MYVIMRC<CR>', {noremap = true})
-vim.keymap.set('n', '<leader>sv', ':w<CR>:so %<CR>:q<CR>', {noremap = true})
+vim.keymap.set('n', '<leader>sv', ':w<CR>:so %<CR>', {noremap = true})
+
+vim.keymap.set('n', '<leader>sc', ':noh<CR>', {noremap = true})
 
 -- netrw
 vim.keymap.set("n", "<leader>pv", vim.cmd.Explore)
@@ -14,6 +16,9 @@ vim.keymap.set("n", "<leader>pv", vim.cmd.Explore)
 vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
 vim.keymap.set("n", "<leader>ga", vim.cmd.Gwrite)
 vim.keymap.set("n", "<leader>gh", ':Git push<CR>')
+vim.keymap.set("n", "<leader>gl", ':Git pull<CR>')
+vim.keymap.set("n", "<leader>gb", ':Git blame<CR>')
+vim.keymap.set("n", "<leader>gV", ':Gvdiffsplit<CR>')
 
 -- undotree
 vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle)
