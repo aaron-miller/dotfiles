a4c5405310c45f1e1cf4cd1e63f19693c5b268cf
Aaron Miller <aaron@aaron-miller.net>
2023-04-15 21:16:24 -0500

    Indentation fix

--- nvim/.config/nvim/after/plugin/lsp.lua
+++ nvim/.config/nvim/after/plugin/lsp.lua
@@ -1,7 +1,7 @@
 local lsp = require('lsp-zero').preset({})
 
 lsp.on_attach(function(client, bufnr)
-	lsp.default_keymaps({buffer = bufnr})
+    lsp.default_keymaps({buffer = bufnr})
 
 
 end)
@@ -12,10 +12,10 @@ require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
 local cmp = require('cmp')
 local cmp_select = {behavior = cmp.SelectBehavior.Select}
 local cmp_mappings = lsp.defaults.cmp_mappings({
-	['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-	['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-	['<C-y>'] = cmp.mapping.confirm({ select = true }),
-	['<C-Space>'] = cmp.mapping.complete(),
+    ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+    ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+    ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+    ['<C-Space>'] = cmp.mapping.complete(),
 })
 
 lsp.setup()
