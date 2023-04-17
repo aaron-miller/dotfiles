d75ed207dd734187ea87f70a998e269423957c7e
Aaron Miller <aaron@aaron-miller.net>
2023-04-16 22:58:11 -0500

    Tweak keymaps

--- nvim/.config/nvim/lua/aaron/remap.lua
+++ nvim/.config/nvim/lua/aaron/remap.lua
@@ -3,34 +3,36 @@ local telescope = require('telescope.builtin')
 vim.g.mapleader = " "
 
 -- standard vim features
-vim.keymap.set("n", "<leader>c", ':bd<CR>', {noremap = true})
-vim.keymap.set("n", "<leader>w", ':w<CR>', {noremap = true})
-vim.keymap.set("n", "<leader>q", ':q<CR>', {noremap = true})
-vim.keymap.set("n", "<leader>Q", ':q!<CR>', {noremap = true})
-vim.keymap.set("n", "<leader><Tab>", '<C-^>', {noremap = true})
+vim.keymap.set("n", "<leader>c", ':bd<CR>')
+vim.keymap.set("n", "<leader>w", ':w<CR>')
+vim.keymap.set("n", "<leader>W", ':wq<CR>')
+vim.keymap.set("n", "<leader>q", ':q<CR>')
+vim.keymap.set("n", "<leader>Q", ':q!<CR>')
+vim.keymap.set("n", "<leader><Tab>", '<C-^>')
 
-vim.keymap.set("n", "<C-J>", "<C-W><C-J>", {noremap = true})
-vim.keymap.set("n", "<C-K>", "<C-W><C-K>", {noremap = true})
-vim.keymap.set("n", "<C-L>", "<C-W><C-L>", {noremap = true})
-vim.keymap.set("n", "<C-H>", "<C-W><C-H>", {noremap = true})
+vim.keymap.set("n", "<C-J>", "<C-W><C-J>")
+vim.keymap.set("n", "<C-K>", "<C-W><C-K>")
+vim.keymap.set("n", "<C-L>", "<C-W><C-L>")
+vim.keymap.set("n", "<C-H>", "<C-W><C-H>")
 
-vim.keymap.set("n", "<leader>sc", ":noh<CR>", {noremap = true})
+vim.keymap.set("n", "<leader>sc", ":noh<CR>")
 
 -- netrw
-vim.keymap.set("n", "<leader>pv", vim.cmd.Explore, {noremap = true})
+vim.keymap.set("n", "<leader>pv", vim.cmd.Explore)
 
 -- Telescope
-vim.keymap.set('n', '<leader>ff', telescope.find_files, {noremap = true})
-vim.keymap.set('n', '<leader>fg', telescope.live_grep, {noremap = true})
-vim.keymap.set('n', '<leader>fb', telescope.buffers, {noremap = true})
-vim.keymap.set('n', '<leader>fh', telescope.help_tags, {noremap = true})
-vim.keymap.set('n', '<leader>ft', telescope.tags, {noremap = true})
+vim.keymap.set('n', '<leader>ff', telescope.find_files)
+vim.keymap.set('n', '<leader>fg', telescope.live_grep)
+vim.keymap.set('n', '<leader>fb', telescope.buffers)
+vim.keymap.set('n', '<leader>fh', telescope.help_tags)
+vim.keymap.set('n', '<leader>ft', telescope.tags)
 
-vim.keymap.set('n', '<C-p>', telescope.git_files, {})
+vim.keymap.set('n', '<C-p>', telescope.git_files)
 
 -- Fugitive/Git/GitHub
 vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
 vim.keymap.set("n", "<leader>ga", vim.cmd.Gwrite)
+vim.keymap.set("n", "<leader>gC", ':Git commit<CR>')
 vim.keymap.set("n", "<leader>gh", ':Git push<CR>')
 vim.keymap.set("n", "<leader>gl", ':Git pull<CR>')
 vim.keymap.set("n", "<leader>gb", ':Git blame<CR>')
