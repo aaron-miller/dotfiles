fde9d0f2b65f7664ca58866694ea5fceb06e4f08
Aaron Miller <aaron@aaron-miller.net>
2023-04-17 00:09:33 -0500

    Added snippets to nvim

--- nvim/.config/nvim/lua/aaron/packer.lua
+++ nvim/.config/nvim/lua/aaron/packer.lua
@@ -8,14 +8,14 @@ return require('packer').startup(function(use)
     use 'folke/tokyonight.nvim'
 
     -- tpope
-    use 'tpope/vim-fugitive'    -- Git
-    use 'tpope/vim-rhubarb'     -- GitHub
+    use 'tpope/vim-fugitive' -- Git
+    use 'tpope/vim-rhubarb' -- GitHub
     use 'tpope/vim-speeddating' -- Increase/decrease dates
-    use 'tpope/vim-sleuth'      -- Figure out file indent settings
-    use 'tpope/vim-obsession'   -- session management manager
-    use 'tpope/vim-repeat'      -- super power .
-    use 'tpope/vim-surround'    -- change surrounding chars like parens and quotes
-    use 'tpope/vim-commentary'  -- comment out a line with gc
+    use 'tpope/vim-sleuth' -- Figure out file indent settings
+    use 'tpope/vim-obsession' -- session management manager
+    use 'tpope/vim-repeat' -- super power .
+    use 'tpope/vim-surround' -- change surrounding chars like parens and quotes
+    use 'tpope/vim-commentary' -- comment out a line with gc
 
     -- other vimscript plugins
     use 'mbbill/undotree'
@@ -46,21 +46,32 @@ return require('packer').startup(function(use)
             -- LSP Support
             {'neovim/nvim-lspconfig'}, -- Required
             { -- Optional
-            'williamboman/mason.nvim',
-            run = function() pcall(vim.cmd, 'MasonUpdate') end
-        }, {'williamboman/mason-lspconfig.nvim'}, -- Optional
-        -- Autocompletion
-        {'hrsh7th/nvim-cmp'}, -- Required
-        {'hrsh7th/cmp-nvim-lsp'}, -- Required
-        {'L3MON4D3/LuaSnip'} -- Required
-    },
+                'williamboman/mason.nvim',
+                run = function() pcall(vim.cmd, 'MasonUpdate') end
+            }, {'williamboman/mason-lspconfig.nvim'}, -- Optional
+            -- Autocompletion
+            {'hrsh7th/nvim-cmp'}, -- Required
+            {'hrsh7th/cmp-nvim-lsp'}, -- Required
+            {'L3MON4D3/LuaSnip'} -- Required
+        },
 
-    use {
-        'nvim-lualine/lualine.nvim',
-        requires = {'nvim-tree/nvim-web-devicons', opt = true}
+        use {
+            'nvim-lualine/lualine.nvim',
+            requires = {'nvim-tree/nvim-web-devicons', opt = true}
+        }
+
     }
 
-}
+    -- Snippets
+    use "rafamadriz/friendly-snippets"
+
+	use({
+		"L3MON4D3/LuaSnip",
+		-- follow latest release.
+		tag = "v<CurrentMajor>.*",
+		-- install jsregexp (optional!:).
+		run = "make install_jsregexp"
+	})
 
 end)
 
