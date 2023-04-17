a2d9ab45ea522ee957c3a3247e68e5285c7b26f8
Aaron Miller <aaron@aaron-miller.net>
2023-04-16 20:13:44 -0500

    Fully remove nvim-tree

--- nvim/.config/nvim/lua/aaron/packer.lua
+++ nvim/.config/nvim/lua/aaron/packer.lua
@@ -61,15 +61,6 @@ return require('packer').startup(function(use)
 		requires = { 'nvim-tree/nvim-web-devicons', opt = true }
 	},
 
-	use {
-	  'nvim-tree/nvim-tree.lua',
-	  requires = {
-		  'nvim-tree/nvim-web-devicons', -- optional
-	  },
-	  config = function()
-		  require("nvim-tree").setup {}
-      end
-	},
 }
 
 end)
