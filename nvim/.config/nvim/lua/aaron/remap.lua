93aad8b098e7042b4f855082eb0d70fcd9a7de93
Aaron Miller <aaron@aaron-miller.net>
2023-04-16 20:13:59 -0500

    noremap

--- nvim/.config/nvim/lua/aaron/remap.lua
+++ nvim/.config/nvim/lua/aaron/remap.lua
@@ -9,21 +9,21 @@ vim.keymap.set("n", "<leader>q", ':q<CR>', {noremap = true})
 vim.keymap.set("n", "<leader>Q", ':q!<CR>', {noremap = true})
 vim.keymap.set("n", "<leader><Tab>", '<C-^>', {noremap = true})
 
-vim.keymap.set("n", "<C-J>", "<C-W><C-J>")
-vim.keymap.set("n", "<C-K>", "<C-W><C-K>")
-vim.keymap.set("n", "<C-L>", "<C-W><C-L>")
-vim.keymap.set("n", "<C-H>", "<C-W><C-H>")
+vim.keymap.set("n", "<C-J>", "<C-W><C-J>", {noremap = true})
+vim.keymap.set("n", "<C-K>", "<C-W><C-K>", {noremap = true})
+vim.keymap.set("n", "<C-L>", "<C-W><C-L>", {noremap = true})
+vim.keymap.set("n", "<C-H>", "<C-W><C-H>", {noremap = true})
 
 vim.keymap.set("n", "<leader>sc", ":noh<CR>", {noremap = true})
 
 -- netrw
-vim.keymap.set("n", "<leader>pv", vim.cmd.Explore)
+vim.keymap.set("n", "<leader>pv", vim.cmd.Explore, {noremap = true})
 
 -- Telescope
-vim.keymap.set('n', '<leader>ff', telescope.find_files, {})
-vim.keymap.set('n', '<leader>fg', telescope.live_grep, {})
-vim.keymap.set('n', '<leader>fb', telescope.buffers, {})
-vim.keymap.set('n', '<leader>fh', telescope.help_tags, {})
+vim.keymap.set('n', '<leader>ff', telescope.find_files,{noremap = true})
+vim.keymap.set('n', '<leader>fg', telescope.live_grep,{noremap = true})
+vim.keymap.set('n', '<leader>fb', telescope.buffers,{noremap = true})
+vim.keymap.set('n', '<leader>fh', telescope.help_tags,{noremap = true})
 
 vim.keymap.set('n', '<C-p>', telescope.git_files, {})
 
