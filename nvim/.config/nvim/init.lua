854127da241698c5346528ef5152429a87c333d0
Aaron Miller <aaron@aaron-miller.net>
2024-04-01 19:36:44 -0500

    Setup binds for fugitive

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -11,7 +11,21 @@ vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
 	-- tpope
-	"tpope/vim-fugitive", -- Git
+	{ -- Git
+		"tpope/vim-fugitive",
+		config = function()
+			vim.keymap.set("n", "<leader>gs", vim.cmd.Git, { desc = "[G]it [s]tatus" })
+			vim.keymap.set("n", "<leader>gg", vim.cmd.Gwrite, { desc = "[G]it [g]ood (:w and :Git add)" })
+			vim.keymap.set("n", "<leader>gA", ":Git commit -a<CR>", { desc = "[G]it commit --[a]ll" })
+			vim.keymap.set("n", "<leader>gc", ":Git commit<CR>", { desc = "[G]it [c]ommit" })
+			vim.keymap.set("n", "<leader>gh", ":Git push<CR>", { desc = "[G]it pus[h]" })
+			vim.keymap.set("n", "<leader>gl", ":Git pull<CR>", { desc = "[G]it pul[l]" })
+			vim.keymap.set("n", "<leader>gb", ":Git blame<CR>", { desc = "[G]it [b]lame" })
+			vim.keymap.set("n", "<leader>go", ":Git log<CR>", { desc = "[G]it l[o]g" })
+			vim.keymap.set("n", "<leader>gV", ":Gvdiffsplit<CR>", { desc = "[G]it diff vsplit" })
+			vim.keymap.set("n", "<leader>gO", ":GBrowse<CR>", { desc = "[G]it br[O]wse" })
+		end,
+	},
 	"tpope/vim-rhubarb", -- GitHub
 	"tpope/vim-speeddating", -- Increase/decrease dates
 	"tpope/vim-sleuth", -- Figure out file indent settings
@@ -47,6 +61,7 @@ require("lazy").setup({
 				["<leader>r"] = { name = "[R]ename", _ = "which_key_ignore" },
 				["<leader>s"] = { name = "[S]earch", _ = "which_key_ignore" },
 				["<leader>w"] = { name = "[W]orkspace", _ = "which_key_ignore" },
+				["<leader>g"] = { name = "[G]it", _ = "which_key_ignore" },
 			})
 		end,
 	},
