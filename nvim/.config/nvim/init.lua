ddd06cfe62346a22abe1ab0083dd9e8d67c0a0f6
Aaron Miller <aaron@aaron-miller.net>
2024-04-02 11:26:45 -0500

    Add vim-test and vimux plugins to nvim

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -617,6 +617,8 @@ require("lazy").setup({
 			--    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
 		end,
 	},
+	"preservim/vimux", -- run a temp terminal in tmux via vim
+	"vim-test/vim-test", -- run tests inside of vim, using vimux's temp terminals
 
 	-- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
 	-- init.lua. If you want these files, they are in the repository, so you can just download them and
