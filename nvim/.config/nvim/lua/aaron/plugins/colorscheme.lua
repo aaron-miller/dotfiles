0b44f424a1445486bf6de49c5b792f426241d4f8
Aaron Miller <aaron@aaron-miller.net>
2024-04-11 17:39:01 -0500

    Tweak colorscheme and ui

--- nvim/.config/nvim/lua/aaron/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/aaron/plugins/colorscheme.lua
@@ -2,9 +2,27 @@ return {
 	-- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
 	"catppuccin/nvim",
 	priority = 1000, -- Make sure to load this before all the other start plugins.
-	init = function()
-		vim.cmd.colorscheme("catppuccin-mocha")
-		-- You can configure highlights by doing something like:
-		-- vim.cmd.hi("Comment gui=none")
+	config = function()
+		require("catppuccin").setup({
+			flavour = "macchiato",
+			transparent_background = true,
+			dim_inactive = {
+				enabled = false,
+			},
+			integrations = {
+				cmp = true,
+				dap = true,
+				dap_ui = true,
+				gitsigns = true,
+				treesitter = true,
+				mason = true,
+				notify = true,
+				which_key = true,
+				telescope = {
+					enabled = true,
+				},
+			},
+		})
+		vim.cmd.colorscheme("catppuccin")
 	end,
 }
--- nvim/.config/nvim/lua/aaron/plugins/lazygit.lua
+++ nvim/.config/nvim/lua/aaron/plugins/lazygit.lua
@@ -14,6 +14,6 @@ return {
 	-- setting the keybinding for LazyGit with 'keys' is recommended in
 	-- order to load the plugin when the command is run for the first time
 	keys = {
-		{ "<leader>gl", "<cmd>LazyGit<cr>", desc = "[G]it [L]azy" },
+		{ "<leader>gz", "<cmd>LazyGit<cr>", desc = "[G]it La[z]y" },
 	},
 }
