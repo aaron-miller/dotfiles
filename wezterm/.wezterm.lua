7e35a51e446a2c29153e2dc6434cf66a708b1e82
Aaron Miller <aaron@aaron-miller.net>
2024-12-08 00:46:08 -0600

    Update wezterm config

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -8,10 +8,11 @@ local config = wezterm.config_builder()
 
 -- use Hack with Nerd Font glyphs
 wezterm.font("Hack Nerd Font Mono", { weight = "Regular", stretch = "Normal", style = "Normal" })
-config.font_size = 12
+config.font_size = 14
 
 -- set background opacity
 config.window_background_opacity = .85
+config.macos_window_background_blur = 10
 
 -- set initial size
 config.initial_rows = 36
@@ -22,6 +23,12 @@ config.enable_scroll_bar = true
 
 -- disable quit prompt
 config.window_close_confirmation = 'NeverPrompt'
+
+-- hide the tab bar if there is only one tab
+config.hide_tab_bar_if_only_one_tab = true
+
+-- hide the window decorations
+config.window_decorations = "RESIZE"
 
 -- and finally, return the configuration to wezterm
 return config
