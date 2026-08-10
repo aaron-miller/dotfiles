12238977b7f590d5772a80dab524b717f57be7da
Aaron Miller <aaron.miller@scoutmotors.com>
2026-08-10 11:08:29 -0500

    add markdown editting changes

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -12,6 +12,35 @@ vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "<leader>E", ":Ex<CR>", { desc = "[E]xplore dir (:Ex)" })
 vim.keymap.set("n", "<leader>W", ":w<CR>", { desc = "[W]rite (:w)" })
 
+-- Markdown: format & wrap prose to 80 chars
+vim.keymap.set("n", "<leader>mw", function()
+  require("conform").format({ async = true, lsp_format = "never" })
+end, { desc = "[M]arkdown [w]rap format (80 char)" })
+
+-- Markdown: toggle inline rendering (render-markdown.nvim)
+vim.keymap.set("n", "<leader>mr", function()
+  require("render-markdown").toggle()
+end, { desc = "[M]arkdown [r]ender toggle" })
+
+-- Markdown: toggle centered Zen writing view (80-char column)
+vim.keymap.set("n", "<leader>mz", function()
+  Snacks.zen.zen({
+    center = true,
+    width = 80,
+  })
+end, { desc = "[M]arkdown [z]en (centered)" })
+
+-- Toggle blink.cmp completion popup for the current buffer
+Snacks.toggle({
+  name = "Completion (Buffer)",
+  get = function()
+    return vim.b.completion ~= false
+  end,
+  set = function(state)
+    vim.b.completion = state
+  end,
+}):map("<leader>uk")
+
 -- Git
 vim.keymap.set("n", "<leader>gg", vim.cmd.Gwrite, { desc = "[G]it [g]ood (:w and :Git add)" })
 vim.keymap.set("n", "<leader>gA", ":Git commit -a<CR>", { desc = "[G]it commit --[A]ll" })
--- nvim/.config/nvim/lua/plugins/markdown.lua
+++ nvim/.config/nvim/lua/plugins/markdown.lua
@@ -5,4 +5,28 @@ return {
       enabled = false,
     },
   },
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      formatters = {
+        -- Wrap markdown prose to 80 chars when formatting.
+        -- prepend_args only applies to markdown via formatters_by_ft below.
+        prettier_markdown_wrap = {
+          command = "prettier",
+          args = {
+            "--stdin-filepath",
+            "$FILENAME",
+            "--prose-wrap",
+            "always",
+            "--print-width",
+            "80",
+          },
+        },
+      },
+      formatters_by_ft = {
+        markdown = { "prettier_markdown_wrap" },
+        ["markdown.mdx"] = { "prettier_markdown_wrap" },
+      },
+    },
+  },
 }
